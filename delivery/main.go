628bbc60478ba7a9729040777228f7c25eca8079
Farouq Umar <ufakz99@gmail.com>
2021-10-24 10:07:39 +0100

    refactor: add delivery estimate comments

--- delivery/main.go
+++ delivery/main.go
@@ -9,17 +9,20 @@ import (
 )
 
 func GetDeliveryEstimate(w http.ResponseWriter, r *http.Request) {
+	//Encode request body as JSON
 	reqBody, err := json.Marshal(r.Body)
 	if err != nil {
 		return
 	}
 
+	//Make post request to Gokada API
 	resp, err := http.Post("https://api.gokada.ng/api/developer/order_estimate", "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		fmt.Print(err.Error())
 		return
 	}
 
+	//Read Gokada API response and store data
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	fmt.Printf("%v", string(bodyBytes))
 	if err != nil {
@@ -29,12 +32,15 @@ func GetDeliveryEstimate(w http.ResponseWriter, r *http.Request) {
 
 	defer resp.Body.Close()
 
+	//Convert response to EstimateResponse struct
 	var response EstimateResponse
 	json.Unmarshal(bodyBytes, &response)
 
+	//Set response headers
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
+	//Encode response as JSON
 	json.NewEncoder(w).Encode(response)
 }
 
